ed3a39473715c508fef4ab0077b1881ba018ee79
Wiktor Frątczak <01171649@pw.edu.pl>
2025-06-19 01:36:48 +0200

    Make interface more generic

--- app/domain/trend_analyzer.py
+++ app/domain/trend_analyzer.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import List, Deque
+from typing import Sequence
+
 from app.domain.trendResult import TrendResult
 from app.domain.ueSessionInfo import UeSessionInfo
 
 
 class TrendAnalyzer(ABC):
     @abstractmethod
-    def evaluate(self, history: Deque[UeSessionInfo]) -> TrendResult:
+    def evaluate(self, history: Sequence[UeSessionInfo]) -> TrendResult:
         pass
